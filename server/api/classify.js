300796acaa7a86f9a4b9d19988fc307550f54c5d
william <yebeixiaohai@gmail.com>
2017-09-20 12:09:15 +0800

    fix bug

--- server/api/classify.js
+++ server/api/classify.js
@@ -1,11 +1,12 @@
-/* eslint-disable */
-
+// const express = require('express')
+// const router = express.Router()
 import { Router } from 'express'
 const  router = Router()
 let Classify = require('../models/Classify')
 var bodyParser = require('body-parser')
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 /* eslint-disable */
+// 获取分类
 const users = [
   { name: 'Al' },
   { name: 'Po' },
@@ -13,12 +14,10 @@ const users = [
 ]
 router.get('/classify', (req, res ,next) => {
   res.json(users)
-
   // Classify.find().exec().then((articles) => {
   //   res.send(articles)
   // })
 })
-
 //添加分类
 
 router.post('/classify1', urlencodedParser, (req, res) => {
@@ -29,7 +28,7 @@ router.post('/classify1', urlencodedParser, (req, res) => {
   // res.status(200).send('classify successed')
   console.log(req.body.name)
   console.log('555')
-
+})
 
 // 删除分类
 // 删除文章并删除文章下面的评论
