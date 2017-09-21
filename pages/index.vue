80931de6e6f603ff828c8fbc7312bcccd5c20839
shadow <770993725@qq.com>
2017-09-21 18:20:15 +0800

    index templete

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,42 @@
 <template>
   <section class="container">
-
+    <h1>test</h1>
   </section>
 </template>
 
 <script>
-import axios from '~/plugins/axios'
+  import axios from '~/plugins/axios'
 
-export default {
-  async asyncData () {
-    let { data } = await axios.get('/api/users')
-    console.log('data', data)
-    return { users: data }
-  },
-  head () {
-    return {
-      title: 'Users'
+  export default {
+    layout: 'main',
+    async asyncData () {
+      let { data } = await axios.get('/api/users')
+      console.log('data', data)
+      return { users: data }
+    },
+    head () {
+      return {
+        title: 'Users'
+      }
     }
   }
-}
 </script>
 
 <style scoped>
 
-.title
-{
-  margin: 30px 0;
+  .title
+  {
+    margin: 30px 0;
 
-}
-.users
-{
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-.user
-{
-  margin: 10px 0;
-}
+  }
+  .users
+  {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  .user
+  {
+    margin: 10px 0;
+  }
 </style>
